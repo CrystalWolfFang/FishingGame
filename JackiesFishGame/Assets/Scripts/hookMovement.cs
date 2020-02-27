d1848e9e18709c27915dd9f831d21dbb2a6f7aad
CrystalWolfFang <adudra1@gmail.comm>
2020-02-27 17:34:27 -0500

    Base Game made
    trying to get joy con to work

---
+++ JackiesFishGame/Assets/Scripts/hookMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hookMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetKeyDown("4th Axis))
+		{
+			this.transform.position = this.transform.position + new Vector3(-1, 0, 0);
+		}
+		else if(Input.GetKeyDown("joystick button 1"))
+		{
+			this.transform.position = this.transform.position + new Vector3(1, 0, 0);
+		}
+    }
+}
